95b89ced747cbe58dd17a880fa1a6b0ba49c0e65
TKF12 <1073516818@qq.com>
2022-02-09 14:49:46 +0800

    布局代码优化

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,17 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    collapsed: false, // 菜单栏是否闭合，true是闭合，false是不闭合
   },
   mutations: {
+    reviseCollapsed(state) { // 修改当前是否显示
+      state.collapsed = !state.collapsed;
+    },
   },
   actions: {
+    reviseCollapsed({ commit }) {
+      commit('reviseCollapsed');
+    },
   },
   modules: {
   },
